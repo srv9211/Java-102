4f9068b78cb96ead2bdf4024d6980815089aa35f
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2022-09-01 17:18:38 +0530

    Add files via upload

---
+++ 31-08/Animal.java
@@ -0,0 +1,10 @@
+package java8;
+
+public abstract class Animal {
+	
+	public void eat() {
+		System.out.println("eating");
+	}
+	
+	public abstract void sound();
+}
---
+++ 31-08/Samsung.java
@@ -0,0 +1,10 @@
+package java8;
+
+public class Samsung implements MobileInterface{
+	public void sendSms() {
+		
+	}
+	public void msg() {
+		
+	}
+}
