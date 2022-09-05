71e3cae1d945c84ab024d101ff8924236f110df7
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2022-09-05 16:55:58 +0530

    Add files via upload

---
+++ Assignments/Day3Assesment.java
@@ -0,0 +1,30 @@
+package java8.day3.AssesmentDay3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day3Assesment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+		
+		System.out.println("Initial numbers list: " + numbers);
+		
+		// Q1
+		List<Integer> squareList = numbers.stream().distinct().map(number -> number*number).collect(Collectors.toList());
+		System.out.println("\nQ1: Square of all distinct numbers: " + squareList);
+		
+		// Q2
+		List<Integer> evenList = numbers.stream().filter(number -> number%2 == 0).collect(Collectors.toList());
+		System.out.println("\nQ2: Even numbers exist in the list: " + evenList);
+		
+		// Q3
+		long count = numbers.stream().count();
+		System.out.println("\nQ3: Total number of elements in the list = " + count);
+
+	}
+
+}
